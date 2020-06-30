c9e2e404d1db5d89371a011c04d4a7c879fdde17
lakshmisharath31 <lakshmisharath.31@gmail.com>
2020-06-30 18:48:06 -0400

    exercise3

--- src/exercises/school/Student.java
+++ src/exercises/school/Student.java
@@ -1,49 +1,110 @@
 package school;
 
-import java.util.Scanner;
-
 public class Student {
-        private String name;
-        private int studentId;
-        private int numberOfCredits = 0;
-        private double gpa = 0.0;
+    private static int nextStudentId = 1;
+    private String name;
+    private int studentId;
+    private int numberOfCredits;
+    private double gpa;
+    private String GradeLevel;
+    private char grade;
+    int gradeScore;
 
-        public void getName()
-        {
-            System.out.println(this.name);
+    public Student(String name, int studentId, int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student() {
+
+    }
+
+    public double addGrade(int courseCredits, double grade) {
+
+        if (grade == 'A') {
+            gradeScore = 4;
+            gpa = (courseCredits * gradeScore)/numberOfCredits;
+        } else  if( grade =='B') {
+            gradeScore = 3;
+
+            gpa = (courseCredits * gradeScore) / numberOfCredits;
+
         }
+        return gpa;
+    }
+    public String getGradeLevel() {
+        if (numberOfCredits <= 29) {
+            GradeLevel = "Freshman";
+        } else if ((numberOfCredits >= 30) && (numberOfCredits <= 59)) {
+            GradeLevel = "Sophomore";
+        } else if ((numberOfCredits >= 60) && (numberOfCredits <= 89)) {
+            GradeLevel = "Junior";
 
-        public void setName(String aName)
-        {
-            this.name = aName;
+        } else if ((numberOfCredits >= 90)) {
+            GradeLevel = "Senior";
+
         }
+        return GradeLevel;
+    }
 
-        public void getStudentId()
-        {
+
+    public void getStudentId() {
         System.out.println(this.studentId);
-        }
-
-        public void setStudentId(int astudentId)
-        {
-        this.studentId = astudentId;
-        }
-
-        public void setNumberOfCredits(int anumberOfCredits){
-            this.numberOfCredits=anumberOfCredits;
-        }
-
-        public void getNumberOfCredits(){
-            System.out.println(this.numberOfCredits);
-        }
-
-        public void setGpa(double agpa){
-            this.gpa=agpa;
-        }
-
-        public void getGpa(){
-            System.out.println(this.gpa);
     }
 
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
     }
+
+    public int setNumberOfCredits(int numberOfCredits) {
+        return this.numberOfCredits = numberOfCredits;
+    }
+
+    public double getGpa(double gpa) {
+     return this.gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public char setGrade(char grade) {
+        return this.grade = grade;
+    }
+
+    public int getNumberOfCredits() {
+
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa ;
+    }
+
+    public String studentInfo() {
+        return (this.name + " has a GPA of: " + this.gpa + " GradeLevel: "+ this.getGradeLevel());
+    }
+}
+    //public void addGrade(int courseCredits, double grade) {
+
+
+
+
+            // Update the appropriate fields: numberOfCredits, gpa
+
+
+
+
+
 
 
--- src/exercises/school/StudentRunner.java
+++ src/exercises/school/StudentRunner.java
@@ -4,15 +4,31 @@ import school.Student;
 
 public class StudentRunner {
     public static void main(String[] args){
-        Student student1=new Student();
+        Student student1 = new Student();
+        Student student2 = new Student();
+
         student1.setName("Sam");
         student1.getName();
-        student1.setStudentId(01);
+        student1.setStudentId(1);
         student1.getStudentId();
-        student1.setNumberOfCredits(1);
+        student1.setNumberOfCredits(4);
         student1.getNumberOfCredits();
-        student1.setGpa(4);
-        student1.getGpa();
+        student1.setGrade('A');
+        System.out.println(student1.addGrade(student1.getNumberOfCredits(),student1.getGrade()));
+        //System.out.println("Student name:"+student1.getName()+"  GPA: "+student1.getGpa()+ " Grade Level: "+student1.getGradeLevel());
+
+        System.out.println(student1.studentInfo());
+        student2.setName("Tiger");
+        student2.getName();
+        student2.setStudentId(2);
+        student2.getStudentId();
+        student2.setNumberOfCredits(35);
+        student2.getNumberOfCredits();
+        student2.setGrade('B');
+        System.out.println(student2.addGrade(student2.getNumberOfCredits(),student2.getGrade()));
+        //System.out.println("Student name:"+student1.getName()+"  GPA: "+student1.getGpa()+ " Grade Level: "+student1.getGradeLevel());
+
+        System.out.println(student2.studentInfo());
 
 
     }
